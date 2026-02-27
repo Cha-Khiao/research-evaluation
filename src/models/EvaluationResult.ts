5b2b0beecfdd12514aa1db9f55c87c0b8b0c158a
Thanasuk00 <thanauk4745@gmail.com>
2026-02-27 14:01:05 +0700

    update

--- src/models/EvaluationResult.ts
+++ src/models/EvaluationResult.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, models } from "mongoose";
 
 const resultSchema = new Schema({
   targetGroupId: { type: Schema.Types.ObjectId, ref: "Group", required: true }, // กลุ่มที่ถูกประเมิน
-  scores: { type: Schema.Types.Mixed, required: true }, // เก็บผลคะแนนเป็น JSON เช่น { q1: 10, q2: 5 }
+  scores: { type: Schema.Types.Mixed, required: true }, 
   comment: { type: String }, 
   
 }, { timestamps: true });
